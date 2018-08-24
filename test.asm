f868d34a0864813de483fa18b193e2239584de47
Trevor Jay <tjay@montrose.is>
2018-08-24 16:17:18 +0000

    removed misleading newlines

--- test.asm
+++ test.asm
@@ -1,24 +1,24 @@
 ;simple ROM that should flash all (non-bs) segments
 SBTBL   equ     $0
 START   equ     $0DC0
-
+;
 ;Indirect Index Table:
 org     SBTBL
         .word   $0 ; $100 for TM, SETHIGH
-
+;
 SETHIGH equ     $0
 org     $100
 R_LOOP  LAX     $F
         EXCI    $0
         T       R_LOOP
         RTN0
-
+;
 org     START
         LBL     $60 ; page R 
         TM      SETHIGH
         LB      $3 ; page S (thanks to the BM bits from above)
         TM      SETHIGH
-
+;
 ; toggle backplane between $0 and $F
         LB      $0
         LAX     $F
